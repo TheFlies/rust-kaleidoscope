8d2f70b6377b32fd742d63e48e8a52e18ab6615b
gaconkzk <gaconkzk@gmail.com>
2019-04-08 11:57:46 +0700

    working on my l

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,12 +5,17 @@ use std::str::Chars;
 pub enum Token {
     // Commands
     Define,
+    Having,
+    Assigned,
     Extern,
+    Block,
+    EndLine,
     // Identifier contains the identifier as a String.
     Identifier(String),
     // Simple version, all num in Kaleidoscope are 64 bit floats
     // We store number in the variant instead of in a global variable.
     Number(f64),
+    String(String),
     // UnknownChar corresponds to returning a positive integer from gettok.
     UnknownChar(char),
 }
@@ -30,7 +35,7 @@ impl<'a> Iterator for Lexer<'a> {
     fn next(&mut self) -> Option<Token> {
         // next is first char that not equal whitespace
         let next = self.chars
-            .find(|&c| !c.is_whitespace());
+            .find(|&c| c != ' ');
 
         match next {
             Some(c) => self.get_token(c),
@@ -59,9 +64,16 @@ impl <'a> Lexer<'a> {
             }
 
             match iden.as_str() {
-                "def" => Some(Token::Define),
+                "create" => Some(Token::Define),
                 "extern" => Some(Token::Extern),
+                "in" => Some(Token::Block),
                 _ => Some(Token::Identifier(iden)),
+            }
+        } else if c == '\'' {
+            let xp = self.chars.next();
+            match xp {
+                Some(c) if c == 's' => Some(Token::Having),
+                _ => Some(Token::UnknownChar(c)),
             }
         } else if c.is_digit(10) || c == '.' {
             let mut num = String::new();
@@ -80,6 +92,23 @@ impl <'a> Lexer<'a> {
                 self.chars.next();
             }
             Some(Token::Number(num.parse().expect("Can't parse number")))
+        } else if c == '"' {
+            let mut iden = String::new();
+            loop {
+                let xp = self.chars.peek();
+                match xp {
+                    Some(c) if *c != '"' => iden.push(*c),
+                    _ => break,
+                };
+                self.chars.next();
+            }
+            // after loop, we next again to bypass the end '"'
+            self.chars.next();
+            Some(Token::String(iden))
+        } else if c == '=' {
+            Some(Token::Assigned)
+        } else if c == '\n' {
+            Some(Token::EndLine)
         } else if c == '#' {
             loop {
                 // We create nested block so xp will be out of scope
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use rkaley::lexer::Lexer;
+use rkaley::parser::Parser;
 
 fn main() {
     let sample = r#"
-    # Compute the x'th fibonacci number.
-    def fib(x)
-      if x < 3 then
-        1
-      else
-        fib(x-1)+fib(x-2)
-
-    # This expression will compute the 40th number.
-    fib(40)
+    # print a hello world
+    create game
+    game's width = 500
+    game's height = 600
+    in game
+      print "hello world!"
     "#;
-    let lex = Lexer::new(sample);
-    lex.for_each(|tok| println!("tok: {:?}", tok));
+    // let lex = Lexer::new(sample);
+    // lex.for_each(|tok| println!("tok: {:?}", tok));
+    let par = &mut Parser::from_source(sample);
+    let ast = &mut par.parse_definition().unwrap();
+    println!("ast: {:?}", ast);
 }
